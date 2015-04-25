25898d3be59f4bc9db9f32f1b329585be361fdca
LiamFengLin <linfeng@berkeley.edu>
2015-04-25 15:15:56 -0700

    fixed db issue on heroku

--- db/migrate/20150415041716_change_mandatory_in_events.rb
+++ db/migrate/20150415041716_change_mandatory_in_events.rb
@@ -1,5 +1,5 @@
 class ChangeMandatoryInEvents < ActiveRecord::Migration
   def change
-    change_column :events, :mandatory, :boolean
+    change_column :events, :mandatory, 'boolean USING CAST(mandatory AS boolean)'
   end
 end
--- db/migrate/20150416194554_add_fields_to_event_assignments.rb
+++ db/migrate/20150416194554_add_fields_to_event_assignments.rb
@@ -1,5 +1,5 @@
 class AddFieldsToEventAssignments < ActiveRecord::Migration
   def change
-    add_column :event_assignments, :mandatory, 'boolean USING CAST(mandatory AS boolean)'
+    add_column :event_assignments, :mandatory, :boolean
   end
 end
